8d5cee1b15f0e66b1d803e1027c9447be4b8b2ca
Julen P. <julenalejandro.perezac@elorrieta-errekamari.com>
2025-12-12 18:59:06 +0100

    Add Batalla Naval and JS exercise files
    Added new Batalla Naval project with HTML, JS, and CSS files. Included two new JavaScript exercise files (I-01 and I-02) with corresponding HTML pages. Updated Tareas.html to link to the new Batalla Naval project.

---
+++ tareas/BNBR/js/js.js
@@ -0,0 +1,7 @@
+function muerte(){
+    document.getElementById("a1").addEventListener("click", crear);
+}
+
+function crear(){
+    window.close();
+}
---
+++ tareas/TareasJs/I-02.js
@@ -0,0 +1,20 @@
+/* 02.01 */
+
+alert("Hola Mundo!\nMira que facil es utilizar 'comillas simples' y \"comillas dobles\"\n(mentira)");
+
+/* 02.02*/
+
+var culo = prompt("Como te llamas?");
+alert("Hola "+culo+", bienvenido a tu web");
+
+/* 02.03 */
+
+function edad(){
+    var edad = prompt("Solicito tu edad, "+culo);
+    alert("Gracias por la respuesta");
+    document.getElementById("txtedad").innerHTML = "Tienes "+edad+" años";
+}
+
+/* 02.04 */
+
+var numeroEntero;
